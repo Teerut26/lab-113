1569731cea9edcf62277dfce3f059e25870b6f2a
Teerut Srithongdee <>
2023-01-09 00:18:57 +0700

    update

--- lab5/03.c
+++ lab5/03.c
@@ -2,10 +2,58 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 int main()
 {
-    
+    float init_value;
+    printf("Initial Value: ");
+    scanf("%f", &init_value);
+
+    int is_continue = 1;
+
+    while (is_continue)
+    {
+        char operator;
+        float input_value = 0;
+        printf("\n");
+        printf("Operator: ");
+        scanf("\n%c", &operator);
+
+        if (operator== '+' || operator== '-' || operator== '*' || operator== '/')
+        {
+            printf("Input Value: ");
+            scanf("%f", &input_value);
+            if (operator== '+')
+            {
+                init_value += input_value;
+                printf("Present Value is %.4f", init_value);
+                printf("\n");
+            }
+            else if (operator== '-')
+            {
+                init_value -= input_value;
+                printf("Present Value is %.4f", init_value);
+                printf("\n");
+            }
+            else if (operator== '*')
+            {
+                init_value *= input_value;
+                printf("Present Value is %.4f", init_value);
+                printf("\n");
+            }
+            else if (operator== '/')
+            {
+                init_value /= input_value;
+                printf("Present Value is %.4f", init_value);
+                printf("\n");
+            }
+        }
+        else
+        {
+            printf("\n");
+            printf("Finish Calculation. Final Value is %.4f", init_value);
+            break;
+        }
+    }
 
     return 0;
 }
--- lab5/06.c
+++ lab5/06.c
@@ -1,36 +1,66 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, a[5], b[5], c[10];
+
+//     printf("Enter array a: \n");
+//     for (i = 0; i < 5; i++)
+//     {
+//         printf("Please enter an integer: ");
+//         scanf("%d", &a[i]);
+//     }
+//     printf("Enter array b: ");
+//     for (i = 0; i < 5; i++)
+//     {
+//         printf("Please enter an integer: ");
+//         scanf("%d", &b[i]);
+//     }
+
+//     for (i = 0; i < 5; i++)
+//     {
+//         c[i] = a[i];
+//     }
+
+//     for (i = 5; i < 10; i++)
+//     {
+//         c[i] = b[i-5];
+//     }
+
+//     printf("Array c: ");
+//     for (i = 0; i < 10; i++)
+//         printf("%d ", c[i]);
+
+//     printf("\n");
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
     int i, a[5], b[5], c[10];
-
-    printf("Enter array a: \n");
     for (i = 0; i < 5; i++)
     {
         printf("Please enter an integer: ");
+
         scanf("%d", &a[i]);
-        printf("%d\n", a[i]);
     }
     printf("Enter array b: \n");
-    for (i = 5; i < 10; i++)
-    {
-        printf("Please enter an integer: ");
-        scanf("%d", &b[i]);
-        printf("%d\n", b[i]);
-    }
-
     for (i = 0; i < 5; i++)
     {
-        // printf("%d", a[i]);
+        printf("Please enter an integer: ");
+
+        scanf("%d", &b[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
         c[i] = a[i];
     }
 
-    printf("\n");
-
     for (i = 5; i < 10; i++)
     {
-        // printf("%d", b[i]);
-        c[i] = b[i];
+        c[i] = b[i - 5];
     }
 
     printf("Array c: ");
